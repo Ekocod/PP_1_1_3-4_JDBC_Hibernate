d20615b80ba9c1a9e6fb9eceeef15a9f0442be77
Eko <ekoslambek@gmail.com>
2022-11-08 05:23:51 +0600

    1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,43 +1,16 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-//import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.util.Util;
-//import org.hibernate.Session;
-//import org.hibernate.SessionFactory;
-//import org.hibernate.cfg.Configuration;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-//        Util.getConnection();
-//        UserDao userDao = new UserDaoJDBCImpl();
-//
-//        //Создание таблицы User(ов)
-//        try {
-//            userDao.createUsersTable();
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
 
-        //Добавление 4 User(ов) в таблицу с данными
-//        userDao.saveUser("Name1", "LastName1", (byte) 20);
-//        userDao.saveUser("Name2", "LastName2", (byte) 25);
-//        userDao.saveUser("Name3", "LastName3", (byte) 31);
-//        userDao.saveUser("Name4", "LastName4", (byte) 38);
+        UserDao userDao = new UserDaoJDBCImpl();
 
-        //Получение всех User из базы и вывод в консоль
-//        System.out.println(userDao.getAllUsers());
-
-        //Очистка таблицы User(ов)
-//        userDao.cleanUsersTable();
-
-        //Удаление таблицы
-//        userDao.dropUsersTable();
-
-        UserDao userDao = new UserDaoHibernateImpl();
+        //Создание таблицы User(ов)
         try {
             userDao.createUsersTable();
         } catch (SQLException e) {
@@ -50,9 +23,6 @@ public class Main {
         userDao.saveUser("Name3", "LastName3", (byte) 31);
         userDao.saveUser("Name4", "LastName4", (byte) 38);
 
-        //Удаление User из таблицы (по id)
-//        userDao.removeUserById(1);
-
         //Получение всех User из базы и вывод в консоль
         System.out.println(userDao.getAllUsers());
 
@@ -61,8 +31,5 @@ public class Main {
 
         //Удаление таблицы
         userDao.dropUsersTable();
-
-        //Закрываем ресурс
-        Util.getSessionFactory().close();
     }
 }
